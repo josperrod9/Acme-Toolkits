524d7898d1897976adaa259edd2dcacc119742aa
pablo <pablo@DESKTOP-3I51TR7>
2022-04-20 01:03:44 +0200

    Task 34:Done.All task done (34-39)

---
+++ src/test/java/acme/testing/authenticated/systemConfiguration/AuthenticatedConfigurationTest.java
@@ -0,0 +1,28 @@
+package acme.testing.authenticated.systemConfiguration;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AuthenticatedConfigurationTest extends TestHarness{
+	
+	// Lifecycle management ---------------------------------------------------
+
+    // Test cases -------------------------------------------------------------
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/configuration/list-configuration.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String defaultCurrency, final String currency) {
+
+		super.signIn("inventor2","inventor2");
+		
+		super.clickOnMenu("authenticated user", "configuration");
+		super.checkFormExists();
+		super.checkInputBoxHasValue("defaultCurrency", defaultCurrency);
+		super.checkInputBoxHasValue("currency", currency);
+		
+		}
+	}
+
